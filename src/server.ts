517e77aa45bbbf7fe0a4d8a75420d7a07ce509b6
gabriel-amorim1 <therealamorim@gmail.com>
2021-05-21 12:42:17 -0300

    config: database and main route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import './database/connection';
+import routes from './routes';
 
 const app = express();
+
+dotenv.config();
+
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running"));
